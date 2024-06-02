bd6accb7a889289c2a642c811679ecee5a442b4f
Radityo <radityohernanda@gmail.com>
2024-06-02 10:13:13 +0700

    remove old text and give space between BigCard and Button.

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,8 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // ← Add this.
           children: [
-            Text(''),
-            Text('A random AWESOME idea:'),
             BigCard(pair: pair),
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 appState.getNext();
