99be6e5a163dca253d857148c2c6ed9a1bd67b95
Radityo <radityohernanda@gmail.com>
2024-06-02 10:04:28 +0700

    change show generated using local variable pair from appState.current.

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;                 // ← Add this.
 
     return Scaffold(
       body: Column(
         children: [
           Text(''),
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),                // ← Change to this.
           ElevatedButton(
             onPressed: () {
               appState.getNext();
