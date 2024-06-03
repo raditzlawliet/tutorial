b2236b85622d7b922b3047995c717dbec24f6ed2
Radityo <radityohernanda@gmail.com>
2024-06-03 19:38:31 +0700

    [flutter-counter-cubit] impl: rework default MyApp into CounterApp [flutter-counter-cubit] impl: prepare and add counter_observer [flutter-counter-cubit] impl: add CounterCubit, CounterPage with placeholder [flutter-counter-cubit] chore: add barrel count.dart to wrap all counter module into single dart [flutter-counter-cubit] chore: remove default test

---
+++ flutter/beginner/flutter-counter-cubit/lib/counter/view/counter_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
