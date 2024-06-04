00f3e86879c5796f667e649aeb30eb5f0fb8cf66
Radityo <radityohernanda@gmail.com>
2024-06-04 13:14:34 +0700

    [flutter-counter-cubit] feat. add counter view [flutter-counter-cubit] impl. counter view to counter page, and barrel all to view.dart and counter.dart module

--- flutter/beginner/flutter-counter-cubit/lib/counter/view/counter_page.dart
+++ flutter/beginner/flutter-counter-cubit/lib/counter/view/counter_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_counter_cubit/counter/counter.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const CounterView(),
+    );
   }
 }
--- flutter/beginner/flutter-counter-cubit/lib/counter/view/view.dart
+++ flutter/beginner/flutter-counter-cubit/lib/counter/view/view.dart
@@ -1,1 +1,2 @@
 export 'counter_page.dart';
+export 'counter_view.dart';
