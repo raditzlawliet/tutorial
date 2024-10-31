46e650ba5c2ae127aac71ae6facbf58e65f34478
Radityo <radityohernanda@gmail.com>
2024-10-31 14:46:51 +0700

    [go/beginner/go-2-api] example group handler

--- golang/beginner/go-2-api/main.go
+++ golang/beginner/go-2-api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,5 +17,55 @@ func main() {
 		})
 	})
 
+	// groping route with /arith
+	arith := r.Group("/arith")
+	{
+		// define GEt route /arith/sum
+		arith.GET("/sum", func(c *gin.Context) {
+			// Query String and parse to int to variable
+			a, _ := strconv.Atoi(c.DefaultQuery("a", "0"))
+			b, _ := strconv.Atoi(c.DefaultQuery("b", "0"))
+
+			// Response the result
+			c.JSON(http.StatusOK, gin.H{
+				"message": sum(a, b),
+			})
+		})
+
+		// defined POST route /arith/sub
+		arith.POST("/sub", arithSubHandler)
+	}
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type arithSubData struct {
+	A int `json:"a" binding:"required"`
+	B int `json:"b" binding:"required"`
+}
+
+func arithSubHandler(c *gin.Context) {
+	var data arithSubData
+
+	// Bind the JSON payload to the User struct
+	if err := c.ShouldBindJSON(&data); err != nil {
+		// Return a 400 Bad Request response with the error message
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Response the result
+	c.JSON(http.StatusOK, gin.H{
+		"message": sub(data.A, data.B),
+	})
+}
+
+// simple arith
+func sum(a int, b int) int {
+	return a + b
+}
+
+// simple arith
+func sub(a int, b int) int {
+	return a - b
 }
