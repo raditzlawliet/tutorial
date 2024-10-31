ace9bccf2da83a4397b9f8f954940d0eca331c39
Radityo <radityohernanda@gmail.com>
2024-10-31 14:45:44 +0700

    [go/beginner/go-2-api] Qyickstart API with Gin

---
+++ golang/beginner/go-2-api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Create new GET route /ping
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
---
+++ golang/beginner/go-2-api/readme.md
@@ -0,0 +1,3 @@
+## Go Beginner - Part 2 - API
+
+Demo Go REST API with Gin
