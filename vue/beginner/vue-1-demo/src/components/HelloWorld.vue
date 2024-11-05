164e2e2208dc52cd58f71bada7d2892c3fac1648
Radityo <radityohernanda@gmail.com>
2024-11-06 02:01:51 +0700

    vue/beginner/vue-1-demo rubah existing component dan nambah routing

--- vue/beginner/vue-1-demo/src/components/HelloWorld.vue
+++ vue/beginner/vue-1-demo/src/components/HelloWorld.vue
@@ -10,11 +10,7 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h3>Project telah berhasil dibuat</h3>
   </div>
 </template>
 
---
+++ vue/beginner/vue-1-demo/src/views/DemoView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main>Demo!</main>
+</template>
