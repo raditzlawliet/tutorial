ab9607eaa6babd324fb1ef2076603217a811fd58
Radityo <radityohernanda@gmail.com>
2024-11-07 00:59:01 +0700

    vue-3-vuetify-login clean project vuetify

---
+++ vue/beginner/vue-3-vuetify-login/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue/beginner/vue-3-vuetify-login/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style scoped></style>
