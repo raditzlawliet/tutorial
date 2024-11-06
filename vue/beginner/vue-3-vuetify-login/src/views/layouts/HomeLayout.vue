698b6877352c420cdfe40253a65a1d85204091d8
Radityo <radityohernanda@gmail.com>
2024-11-07 01:49:06 +0700

    vue-3-vuetify-login: home layout ui

--- vue/beginner/vue-3-vuetify-login/src/views/layouts/HomeLayout.vue
+++ vue/beginner/vue-3-vuetify-login/src/views/layouts/HomeLayout.vue
@@ -1,6 +1,41 @@
 <template>
   <v-responsive>
     <v-app id="app">
+      <v-navigation-drawer v-model="drawer">
+        <!-- sidebar -->
+        <v-sheet class="pa-4" color="primary">
+          <div class="font-weight-bold">Hello, C.C.</div>
+          May the Force be with you
+        </v-sheet>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <RouterLink
+            v-for="{ icon, text, route, click } in sidebarLinks"
+            :key="route"
+            :to="route"
+            custom
+          >
+            <v-list-item
+              :prepend-icon="icon"
+              :title="text"
+              link
+              @click="click"
+              v-if="click"
+            ></v-list-item>
+            <v-list-item :prepend-icon="icon" :title="text" link :to="route" v-else></v-list-item>
+          </RouterLink>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-img class="" max-width="100" :src="logo"></v-img>
+
+        <v-spacer></v-spacer>
+      </v-app-bar>
+
       <v-main id="main">
         <RouterView />
       </v-main>
@@ -8,4 +43,22 @@
   </v-responsive>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import logo from '@/assets/logo-digi.png'
+import { mdiHome, mdiLogout } from '@mdi/js'
+import { RouterView, useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const drawer = ref(null)
+
+function logout() {
+  router.push({ name: 'login' })
+}
+
+const sidebarLinks = [
+  { icon: mdiHome, text: 'Home', route: '/' },
+  { icon: mdiLogout, text: 'Logout', route: '/login', click: logout },
+]
+</script>
