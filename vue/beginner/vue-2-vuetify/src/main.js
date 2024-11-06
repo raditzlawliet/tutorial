973a15cd530622c50aa6e5cfc6ab4e42d38b499c
Radityo <radityohernanda@gmail.com>
2024-11-07 00:15:02 +0700

    vue/beginner/vue-2-vuetify: - install vuetify and mdi icon with sample in home view - add commit script

--- vue/beginner/vue-2-vuetify/src/main.js
+++ vue/beginner/vue-2-vuetify/src/main.js
@@ -1,13 +1,34 @@
 import './assets/main.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Material Design Icon
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
 
+const app = createApp(App)
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
--- vue/beginner/vue-2-vuetify/src/views/HomeView.vue
+++ vue/beginner/vue-2-vuetify/src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { mdiExcavator, mdiCheckCircle, mdiPartyPopper } from '@mdi/js'
+import { ref } from 'vue'
+
+const showDialog = ref(false)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <v-container fluid class="fill-height">
+      <v-row>
+        <v-col cols="12" class="d-flex justify-center">
+          <v-btn :prepend-icon="mdiExcavator" @click="showDialog = true">Click me!</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-dialog max-width="500" v-model="showDialog">
+      <v-card title="Congratulation">
+        <v-card-text> Now you just need to explore it more deeply. </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Ok" @click="showDialog = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </main>
 </template>
